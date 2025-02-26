ec70af6009be2f3219bed6570e1f58b2213202d2
Vtormacs <macskids98play@hotmail.com>
2025-02-26 15:33:24 -0300

    Feat: Prendendo as variaveis

---
+++ Variaveis/variaveis.php
@@ -0,0 +1,24 @@
+<?php
+
+// declaracao de variaveis
+
+$nome = "Vitor eduardo";
+$inteiro = 10;
+$decimal = 10.5;
+$boolean = true;
+$lista = array("Vitor", "Eduardo", "Silva");
+$lsita2 = [10.5, 20.5, 30.5];
+$variavel10 = 10;
+
+echo $boolean;
+
+var_dump($lista);
+
+// constantes
+
+define("PI", 3.14);
+
+
+echo PI;
+
+?>
